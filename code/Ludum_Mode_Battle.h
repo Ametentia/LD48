546d3b2745b2efe1bc681ecb64b03979a2b0e71c
Matt <matt@threlfall.dev>
2021-04-25 18:01:02 +0100

    Battle screen with single call

--- code/Ludum_Mode_Battle.h
+++ code/Ludum_Mode_Battle.h
@@ -1,12 +1,49 @@
 #if !defined(LUDUM_MODE_BATTLE_H_)
 #define LUDUM_MODE_BATTLE_H_
+
+enum Button {
+    Button_Up = 0,
+    Button_Down,
+    Button_Left,
+    Button_Right
+};
+
+struct Call {
+    f32 beats[20];
+    Button beatButtons[20];
+    s8 beat_count;
+    u8 successThreshold;
+    v2 pos[40];
+    u8 visable[20];
+    s8 beat_shown;
+};
+
+struct CallSet {
+    Call calls[5];
+    u32 current_call;
+};
+
 struct Mode_Battle {
     Memory_Allocator *alloc;
-    Memory_Allocator *temp;
     Random random;
+    f32 bpm;
+    f32 intro_wait;
+    f32 timer;
+    s8 wave;
+    u64 beat_wait;
 
     u32 points;
     u8 health;
     u8 done;
+    v2 metronome;
+    f32 met_angle;
+    v2 met_flip_range;
+    f32 met_speed;
+    f32 guide_height;
+    f32 dance_timer;
+    s8 dance_angle;
+    CallSet calls;
+    Image_Handle *beat_textures;
+    Game_Button control_map[4];
 };
 #endif  // LUDUM_MODE_BATTLE_H_
